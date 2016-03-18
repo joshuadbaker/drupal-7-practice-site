e612c472933f612a8c0cdb4fafabee909d7959b9
Joshua Baker <joshuadbaker10@gmail.com>
2016-03-18 18:06:12 -0400

    added Foundation 5 and subtheme

---
+++ sites/all/themes/foundation2/template.php
@@ -0,0 +1,19 @@
+<?php
+
+/**
+ * Implements template_preprocess_html().
+ */
+function foundation2_preprocess_html(&$variables) {
+}
+
+/**
+ * Implements template_preprocess_page.
+ */
+function foundation2_preprocess_page(&$variables) {
+}
+
+/**
+ * Implements template_preprocess_node.
+ */
+function foundation2_preprocess_node(&$variables) {
+}
